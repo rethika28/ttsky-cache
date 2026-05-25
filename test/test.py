6808ba106015a4f31608f365112e638aae19a2b4
rethika28 <rethikasubramani28@gmail.com>
2026-05-25 13:19:59 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,41 +1,47 @@
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import RisingEdge
 
 
 @cocotb.test()
 async def cache_test(dut):
 
+    # Initialize
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.ena.value = 1
-    dut.clk.value = 0
     dut.rst_n.value = 0
+    dut.clk.value = 0
 
-    await Timer(10, units="ns")
+    # Reset
+    for _ in range(2):
+        dut.clk.value = 0
+        await RisingEdge(dut.clk)
 
     dut.rst_n.value = 1
 
     # Write value 25 to address 2
+    # ui_in[7:2] = data
+    # ui_in[1:0] = address
+
     dut.ui_in.value = (25 << 2) | 0b10
     dut.uio_in.value = 1
 
-    # Clock pulse
+    # Clock pulse for write
     dut.clk.value = 1
-    await Timer(10, units="ns")
+    await RisingEdge(dut.clk)
 
     dut.clk.value = 0
-    await Timer(10, units="ns")
 
     # Disable write
     dut.uio_in.value = 0
 
-    # Read address 2
+    # Read from address 2
     dut.ui_in.value = 0b10
 
-    await Timer(10, units="ns")
+    await RisingEdge(dut.clk)
 
-    result = dut.uo_out.value.integer
+    result = int(dut.uo_out.value)
 
     print("Cache Output =", result)
 
-    assert result == 25
+    assert result == 25, f"Expected 25 but got {result}"
